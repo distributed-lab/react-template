8411ebb749b09819bca5017bdf6465871b854a64
lukachi <lukachisama@gmail.com>
2022-07-10 01:12:54 +0300

    input field

--- src/pages/InputsOverview/InputsOverview.tsx
+++ src/pages/InputsOverview/InputsOverview.tsx
@@ -1,7 +1,51 @@
-import { FC, HTMLAttributes } from 'react'
+import './styles.scss'
+
+import { FC, HTMLAttributes, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { InputField } from '@/fields'
 
 const InputsOverview: FC<HTMLAttributes<HTMLDivElement>> = () => {
-  return <div className='inputs-overview'>inputs-overview</div>
+  const [simple, setSimple] = useState<string | number>('')
+
+  const { t } = useTranslation()
+
+  return (
+    <div className='inputs-overview'>
+      <div className='inputs-overview__row'>
+        <div className='inputs-overview__col'>
+          <InputField
+            value={simple}
+            setValue={setSimple}
+            label={t('inputs-overview.some-label')}
+            placeholder={t('inputs-overview.some-placeholder')}
+          />
+          <InputField
+            value={simple}
+            setValue={setSimple}
+            label={t('inputs-overview.some-label')}
+            errorMessage={simple as string}
+            placeholder={t('inputs-overview.some-placeholder')}
+          />
+          <InputField
+            value={simple}
+            setValue={setSimple}
+            label={t('inputs-overview.some-label')}
+            placeholder={t('inputs-overview.some-placeholder')}
+            disabled
+          />
+          <InputField
+            value={simple}
+            setValue={setSimple}
+            label={t('inputs-overview.some-label')}
+            errorMessage={t('inputs-overview.some-error-message')}
+            placeholder={t('inputs-overview.some-placeholder')}
+            disabled
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default InputsOverview
