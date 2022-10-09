2480a512cf966472b5f95a00d88055f94da4b23c
lukachi <lukachisama@gmail.com>
2022-10-09 15:56:43 +0300

    use validations for common use-cases

--- src/forms/LoginForm/LoginForm.tsx
+++ src/forms/LoginForm/LoginForm.tsx
@@ -27,13 +27,36 @@ const LoginForm = () => {
       },
     },
   })
+  const [somePrimitiveArray, setSomePrimitiveArray] = useState([
+    'one',
+    'two',
+    'three',
+  ])
+  const [someObjectArray, setSomeObjectArray] = useState([
+    {
+      id: '1',
+      name: 'one',
+    },
+    {
+      id: '2',
+      name: 'two',
+    },
+    {
+      id: '3',
+      name: 'three',
+    },
+  ])
 
   const { isFormDisabled, disableForm, enableForm } = useForm()
   const { isFormValid, getFieldErrorMessage, touchField } = useFormValidation(
-    { login, password, someComplexObject },
+    { login, password, someComplexObject, somePrimitiveArray, someObjectArray },
     {
       login: { required },
-      password: { required, minLength: minLength(6), maxLength: maxLength(32) },
+      password: {
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(32),
+      },
       someComplexObject: {
         required,
         fullName: {
@@ -49,6 +72,20 @@ const LoginForm = () => {
             mother: { required, minLength: minLength(6) },
             father: { required },
           },
+        },
+      },
+      somePrimitiveArray: {
+        required,
+        $every: {
+          required,
+          minLength: minLength(10),
+        },
+      },
+      someObjectArray: {
+        required,
+        $every: {
+          id: { required },
+          name: { required, minLength: minLength(10) },
         },
       },
     },
@@ -71,6 +108,7 @@ const LoginForm = () => {
 
   return (
     <form onSubmit={submit} className='login-form'>
+      <h3>{t('login-form.primitives-title')}</h3>
       <InputField
         value={login}
         setValue={setLogin}
@@ -88,6 +126,7 @@ const LoginForm = () => {
         onBlur={() => touchField('password')}
         disabled={isFormDisabled}
       />
+      <h3>{t('login-form.nested-object-title')}</h3>
       <pre>{JSON.stringify(someComplexObject)}</pre>
       <input
         type='text'
@@ -110,6 +149,59 @@ const LoginForm = () => {
       <span>
         {getFieldErrorMessage('someComplexObject.fullName.firstName')}
       </span>
+      <h3>{t('login-form.primitive-array-title')}</h3>
+      {somePrimitiveArray.map((item, index) => (
+        <InputField
+          key={`primitive-array-${index}`}
+          value={item}
+          onInput={e => {
+            setSomePrimitiveArray(prev => {
+              const next = [...prev]
+              next[index] = e.currentTarget?.value || ''
+              return next
+            })
+          }}
+          errorMessage={getFieldErrorMessage(`somePrimitiveArray[${index}]`)}
+          onBlur={() => touchField(`somePrimitiveArray[${index}]`)}
+        />
+      ))}
+      <h3>{t('login-form.object-array-title')}</h3>
+      {someObjectArray.map((item, index) => (
+        <div className='login-form__controls' key={`object-array-${index}`}>
+          <InputField
+            value={item.id}
+            onInput={e => {
+              setSomeObjectArray(prev => {
+                const next = [...prev]
+                next[index] = {
+                  ...next[index],
+                  id: e.currentTarget?.value || '',
+                }
+                return next
+              })
+            }}
+            errorMessage={getFieldErrorMessage(`someObjectArray[${index}].id`)}
+            onBlur={() => touchField(`someObjectArray[${index}].id`)}
+          />
+          <InputField
+            value={item.name}
+            onInput={e => {
+              setSomeObjectArray(prev => {
+                const next = [...prev]
+                next[index] = {
+                  ...next[index],
+                  name: e.currentTarget?.value || '',
+                }
+                return next
+              })
+            }}
+            errorMessage={getFieldErrorMessage(
+              `someObjectArray[${index}].name`,
+            )}
+            onBlur={() => touchField(`someObjectArray[${index}].name`)}
+          />
+        </div>
+      ))}
       <div className='login-form__actions'>
         <AppButton
           text={t('login-form.submit-btn')}
