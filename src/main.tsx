17c9ca1926334f0bd1126d8dee4586de1736408d
lukachi <lukachisama@gmail.com>
2023-06-25 00:57:14 +0300

    refactored store with Valtio

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,13 @@ import 'virtual:svg-icons-register'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider as StoreProvider } from 'react-redux'
 
 import { App } from '@/App'
-import { store } from '@/store'
 
 const root = createRoot(document.getElementById('root') as Element)
 
 root.render(
   <StrictMode>
-    <StoreProvider store={store}>
-      <App />
-    </StoreProvider>
+    <App />
   </StrictMode>,
 )
